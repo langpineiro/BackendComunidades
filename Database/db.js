33aad66b576ededc3cb1ab515ff2bb5983b46f9c
langpineiro <langpineiro@gmail.com>
2022-11-12 19:04:00 -0600

    env

--- Database/db.js
+++ Database/db.js
@@ -1,7 +1,11 @@
 const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('bxtzuwybmaiie3qrjodr', 'upyu3xcvtab2gupi', 'LfPgGMZXNXP1ZcKeLfzC', {
-  host: 'bxtzuwybmaiie3qrjodr-mysql.services.clever-cloud.com',
+require('dotenv').config();
+const {DB_USER,
+    DB_PASSWORD,
+    DB_HOST,
+    DB_NAME} = process.env;
+const sequelize = new Sequelize(`${DB_NAME}`, `${DB_USER}`, `${DB_PASSWORD}`, {
+  host: `${DB_HOST}`,
   dialect: 'mysql',
 });
  sequelize.authenticate().then(() => {
